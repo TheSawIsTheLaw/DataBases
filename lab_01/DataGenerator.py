3ca032142ee2c3c36adc8d48cf8f3b65b73407fc
TheSawIsTheLaw <dimakrok@bk.ru>
2020-09-12 13:06:40 +0300

    ADD: end of lab

--- lab_01/DataGenerator.py
+++ lab_01/DataGenerator.py
@@ -1,22 +1,23 @@
 from faker import *
-from random import randint, choice
+from random import randint
 
 ZZZAP = 1000
 
 def generateDebtor():
     faker = Faker()
+    f.write("LoanID;BankID;HangmanID;FirstName;SecondName;PassportNum;TelephoneNum;HomeAddress\n")
     f = open('debtors.csv', 'w')
     for i in range(ZZZAP):
-        DeptorID = i
         LoanID = i
         BankID = randint(0, ZZZAP)
         HangmanID = randint(0, ZZZAP)
+        FirstName = faker.first_name()
         FirstName = faker.first_name()
         SecondName = faker.last_name()
         PassportNum = randint(1000000000, 9999999999)
         TelephoneNum = randint(80000000000, 89999999999)
         HomeAddress = faker.address()
-        line = "{};{};{};{};{};{};{};{};{} {}\n".format(DeptorID, LoanID, BankID, HangmanID, FirstName, SecondName, PassportNum, str(TelephoneNum), HomeAddress.split('\n')[0], HomeAddress.split('\n')[1])
+        line = "{};{};{};{};{};{};{};{} {}\n".format(LoanID, BankID, HangmanID, FirstName, SecondName, PassportNum, str(TelephoneNum), HomeAddress.split('\n')[0], HomeAddress.split('\n')[1])
         f.write(line)
     f.close()
 
@@ -30,7 +31,7 @@ def generateHangman():
         FirstName = faker.first_name()
         LastName = faker.last_name()
         TelephoneNum = randint(80000000000, 89999999999)
-        line = "{};{};{};{};{};{}\n".format(HangmanID, Chief, Position, FirstName, LastName, TelephoneNum)
+        line = "{};{};{};{};{}\n".format(Chief, Position, FirstName, LastName, TelephoneNum)
         f.write(line)
     f.close()
 
@@ -38,12 +39,11 @@ def generateLoanSubject():
     faker = Faker()
     f = open("loansubjects.csv", 'w')
     for i in range(ZZZAP):
-        LoanID = i
         SubjectName = faker.word()
         Debt = randint(500, 1000000)
         PurchaseDate = faker.date()
         Price = randint(5000, 9999999)
-        line = "{};{};{};{};{}\n".format(LoanID, SubjectName, Debt, PurchaseDate, Price)
+        line = "{};{};{};{}\n".format(SubjectName, Debt, PurchaseDate, Price)
         f.write(line)
     f.close()
 
@@ -51,13 +51,12 @@ def generateRelatives():
     faker = Faker()
     f = open("relatives.csv", 'w')
     for i in range(ZZZAP * 2):
-        RelativeID = i
         DeptorID = randint(0, ZZZAP)
         FirstName = faker.first_name()
         LastName = faker.last_name()
         TelephoneNum = randint(80000000000, 89999999999)
         HomeAddress = faker.address()
-        line = "{};{};{};{};{};{} {}\n".format(RelativeID, DeptorID, FirstName, LastName, TelephoneNum,
+        line = "{};{};{};{};{} {}\n".format(DeptorID, FirstName, LastName, TelephoneNum,
                                              HomeAddress.split('\n')[0], HomeAddress.split('\n')[1])
         f.write(line)
     f.close()
@@ -66,11 +65,12 @@ def generateBanks():
     faker = Faker()
     f = open("banks.csv", 'w')
     for i in range(ZZZAP):
-        BankID = i
         Name = faker.word()
         Mail = faker.email()
         TelephoneNum = randint(80000000000, 89999999999)
-        line = "{};{};{};{}\n".format(BankID, Name, Mail, TelephoneNum)
+        LegalAddress = faker.address()
+        line = "{};{};{};{} {}\n".format(Name, Mail, TelephoneNum,
+                                      LegalAddress.split('\n')[0], LegalAddress.split('\n')[1])
         f.write(line)
     f.close()
 
