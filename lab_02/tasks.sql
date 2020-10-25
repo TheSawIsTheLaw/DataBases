2e40e13aceb81b467337491336428e21d76082d4
TheSawIsTheLaw <dimakrok@bk.ru>
2020-10-25 13:52:44 +0300

    ADD: new zaproseki((((

--- lab_02/tasks.sql
+++ lab_02/tasks.sql
@@ -37,9 +37,49 @@ where NOT EXISTS(
     select RELATIVES.DEBTORID
     from RELATIVES
     where DEBTORS.DEBTORID = RELATIVES.DEBTORID
-          )
+          );
 
 -- Проверка
 select *
 from RELATIVES
 where DEBTORID = 579
+
+-- Инструкция select, использующая предикат сравнения с квантором
+-- Все объекты долга, которые дешевле всех объектов долга, приобретённых в 2000 году
+select SUBJECTNAME, DEBT, PURCHASEDATE
+from LOANSUBJECTS
+where DEBT < all(
+    select DEBT
+    from LOANSUBJECTS
+    where PURCHASEDATE > '2000-01-01' and PURCHASEDATE < '2000-12-31'
+    );
+
+-- Проверка
+select *
+from LOANSUBJECTS
+where PURCHASEDATE > '2000-01-01' and PURCHASEDATE < '2000-12-31';
+
+-- Инструкция SELECT, использующая агрегатные функции в выражениях столбцов
+-- Получаем и вычисляем средний размер долга по задолженностям
+select AVG(DEBT) as avgFunc, SUM(DEBT) / COUNT(*) as calculatedAvg
+from LOANSUBJECTS;
+
+-- Инструкция select, использующая скалярные подзапросы в выражениях столбцов
+-- Вывести имена всех палачей, средний долг их "клиентов" и количество их клиентов
+select HANGMANID, FIRSTNAME, LASTNAME,
+       (
+           select AVG(DEBT)
+           from DEBTORS join HANGMAN H on DEBTORS.HANGMANID = H.HANGMANID join LOANSUBJECTS L on DEBTORS.LOANID = L.LOANID
+           where H.HANGMANID = HANGMAN.HANGMANID
+           ) as averageDebtOfDebtors,
+       (
+           select COUNT(*)
+           from DEBTORS
+           where DEBTORS.HANGMANID = HANGMAN.HANGMANID
+           ) as sumOfDebtors
+from HANGMAN;
+
+-- Проверка на то, что у Бритттани Вагнер нет "клиентов"
+select *
+from DEBTORS
+where HANGMANID = 1
