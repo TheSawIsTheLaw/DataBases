a93bb25beed923986a89cb0691037933ec004aa1
TheSawIsTheLaw <dimakrok@bk.ru>
2020-11-16 00:57:54 +0300

    ADD: AZINO FUCK SATANA

---
+++ lab_03/7_cursor.sql
@@ -0,0 +1,27 @@
+-- Я так и не понял, зачем нам нужны курсоры, если мы в процедурах выполняем действия над таблицами.
+-- Удобно, конечно, но толк?..
+
+-- UPD: я тут узнал, что select into, insert, update и delete тоже курсорами являются. Забавно. Теперь я понял.
+
+create or replace procedure showAllExecutorsNames
+is
+    curName varchar2(64);
+begin
+    declare
+        cursor hangmanCursor is
+            select *
+            from HANGMAN;
+
+    begin
+        for curHangman in hangmanCursor
+        loop
+            curName := curHangman.LASTNAME;
+            DBMS_OUTPUT.PUT_LINE(curName);
+        end loop;
+    end;
+end;
+
+-- ВАЖНО! ПЕРЕД ИСПОЛЬЗОВАНИЕМ ВКЛЮЧИТЬ ВЫВОД ДБМС ДЕБАГА
+begin
+    showAllExecutorsNames();
+end;
