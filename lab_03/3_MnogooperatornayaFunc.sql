c57ac2d1cb9a795944ae61c21f8a6a0efa899b89
TheSawIsTheLaw <dimakrok@bk.ru>
2020-11-15 12:09:59 +0300

    ADD: billi djin nasral v kuvshin OOPA

--- lab_03/3_MnogooperatornayaFunc.sql
+++ lab_03/3_MnogooperatornayaFunc.sql
@@ -1,9 +1,29 @@
-create or replace function getDebtorsWithPurchaseDateAfter(afterDate in date)
-return TABLETYPE as tableType;
+-- Ну да, описать тип возвращаемой таблицы внутри функции тоже нельзя.
+-- Поэтому мы будем делать предыдущий запрос, но в ином ключе. Класс.
+
+create or replace type rowLoansPriceLower is object(
+    DEBTORLASTNAME varchar2(64),
+    LOANID number,
+    SUBJECTNAME varchar2(256),
+    PRICE number
+);
+drop type rowLoansPriceLower;
+
+create or replace type LoansLowerTableType is table of rowLoansPriceLower;
+drop type LoansLowerTableType;
+
+
+-- Функция, возвращающая фамилии всех должников, цена преобретённых
+-- предметов у которых ниже заданной.
+create or replace function getDebtorsWithLoanPriceInRange(startPrice in number, endPrice in number)
+return LoansLowerTableType as retTable LoansLowerTableType;
     begin
-         select rowDebtorsPurchaseDateAfter(FIRSTNAME, LASTNAME, TO_DATE(PURCHASEDATE, 'YYYY-MM-DD'))
-         bulk collect into retTable
-         from SYSTEM.DEBTORS D join SYSTEM.LOANSUBJECTS L on L.LOANID = D.LOANID
-         where TO_DATE(L.PURCHASEDATE, 'YYYY-MM-DD') > afterDate;
-         return retTable;
+        select rowLoansPriceLower(D.LASTNAME, L.LOANID, L.SUBJECTNAME, L.PRICE)
+        bulk collect into retTable
+        from DEBTORS D join LOANSUBJECTS L on D.LOANID = L.LOANID
+        where PRICE < endPrice and PRICE > startPrice;
+        return retTable;
     end;
+
+select *
+from getDebtorsWithLoanPriceInRange(60000, 100000);
